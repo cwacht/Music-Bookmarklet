feab36b396ee56bc39b10ae46ffabe8af680925b
cwacht <chris@mobomo.com>
2018-01-18 16:07:15 -0500

    Style file and various tweaks
    Abstracted css out into a separate file which gets compressed and injected into the script.
    
    Overhauled player design.
    Updated button icons.
    Added search button.
    Increased size of player components for touch screen usability.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 var gulp = require('gulp');
 
 /*
-inject uses gulp-inject to splice the minified javascript (bookmarklet.min.js) into our src in place of the starttag and endtag.
-It is configured to remove the tags and directly inject the javascript as a string
-It runs whenever the ./src/index.html or ./dest/bookmarklet.min.js are changed
+inject uses gulp-inject to splice the minified javascript (bookmarklet.min.js) into dest/index.html
+configured to remove the tags and directly inject the javascript as a string
+runs when ./src/index.html or ./dest/bookmarklet.min.js are changed
 */
 gulp.task('inject', function () {
 	var inject = require('gulp-inject');
@@ -20,14 +20,32 @@ gulp.task('inject', function () {
 });
 
 /*
+inject-css uses gulp-inject to splice the minified css (style.min.css) into dest/bookmarklet.js
+configured to remove the tags and directly inject the style as a string
+runs when ./dest/style.min.css or ./src/bookmarklet.js are changed
+*/
+gulp.task('inject-css', function () {
+	var inject = require('gulp-inject');
+	gulp.src('./src/bookmarklet.js').pipe(inject(gulp.src(['./dest/style.min.css']), {
+    starttag: '/*inject:css*/',
+	  endtag: '/*endinject*/',
+		removeTags: true,
+    transform: function (filePath, file) {
+      return file.contents.toString('utf8')
+    }
+  }))
+  .pipe(gulp.dest('./dest'));
+});
+
+/*
 compress uses gulp-uglify and gulp-rename to minify the bookmarklet.js into bookmarklet.min.js
-It is configured to mangle the script by shortening variable names and function names.
-It runs whenever ./src/bookmarklet.js is changed
+configured to mangle the script by shortening variable names and function names.
+runs whenever ./src/bookmarklet.js is changed
 */
 gulp.task('compress', function () {
 	var uglify = require('gulp-uglify');
 	var rename = require('gulp-rename');
-  return gulp.src('./src/bookmarklet.js')
+  return gulp.src('./dest/bookmarklet.js')
     .pipe(uglify({
 			mangle: {
 				toplevel: true
@@ -39,13 +57,32 @@ gulp.task('compress', function () {
 		.pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./dest'));
 });
+/*
+compress uses gulp-clean-css and gulp-rename to minify the style.css into style.min.css
+runs whenever ./src/style.css is changed
+*/
+gulp.task('compress-css', function () {
+	var cleanCSS = require('gulp-clean-css');
+	var rename = require('gulp-rename');
+  return gulp.src('./src/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dest'));
+});
 
 
 gulp.task('watch', function(){
-	gulp.watch('./src/bookmarklet.js', ['compress']); 
+	gulp.watch('./src/style.css', ['compress-css']); 
+	gulp.watch('./dest/style.min.css', ['inject-css']); 
+	gulp.watch('./src/bookmarklet.js', ['inject-css']); 
   gulp.watch('./src/index.html', ['inject']); 
+	gulp.watch('./dest/bookmarklet.js', ['compress']);  
 	gulp.watch('./dest/bookmarklet.min.js', ['inject']);  
 });
 
 
-gulp.task('default', ['compress', 'inject', 'watch']);
+gulp.task('default', ['compress-css', 'inject-css', 'compress', 'inject', 'watch']);
+
+//if css changed, compress css, inject css
+//if js changed, compress js, inject js
+//if html is changed, inject js
